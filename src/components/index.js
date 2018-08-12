e21eb407234652a49c8cb902a89272c937374bb6
Dilrong <wet932@naver.com>
2018-08-12 10:20:34 +0900

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header/Header';
-import CounselorCard from './CounselorCard/CounselorCard';
-
-export {
-    Header,
-    CounselorCard
-}
--- src/containers/index.js
+++
@@ -1,7 +0,0 @@
-import CounselorContainer from '../containers/CounselorContainer/CounselorContainer'
-import SubmitContainer from '../containers/SubmitContainer/SubmitContainer'
-
-export {
-    CounselorContainer,
-    SubmitContainer
-}
